48c7a5c6a47e3e7c80fa6b76ea9b52117903eb65
HuskyKingdom <1595025647@qq.com>
2023-02-27 10:12:50 +0000

    fixing bugs

--- runners/nonadaptivea3c_train.py
+++ runners/nonadaptivea3c_train.py
@@ -83,7 +83,7 @@ def nonadaptivea3c_train(
             if compute_grad:
                 # Compute gradient.
                 player.model.zero_grad()
-                print("Total_loss = {}".format(loss["total_loss"]))
+                print("Total_loss = {}, reward = {}".format(loss["total_loss"],total_reward))
                 loss["total_loss"].backward()
                 torch.nn.utils.clip_grad_norm_(player.model.parameters(), 100.0)
                 # Transfer gradient to shared model and step optimizer.
